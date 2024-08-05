a4cff85cfa008d34011018f56ee2df71c220677a
gdbmrm <>
2024-08-05 12:01:06 +0000

    correction ex 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -21,18 +21,14 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 	new_node->n = value;
-	new_node->left = NULL;
 	new_node->parent = parent;
+	new_node->right = parent->right;
+	new_node->left = NULL;
 
-	if (parent->right == NULL)
-	{
-		parent->right = new_node;
-		new_node->right == NULL;
-	} else
-	{
-		new_node->right = parent->right;
-		parent->right = new_node;
-	}
+	if (parent->right != NULL)
+		parent->right->parent = new_node;
+
+	parent->right = new_node;
 
 	return (new_node);
 }
