6dadcca6865b3b7afd061442b5f68e4f6ba44084
Ema Decot <emadecot03@gmail.com>
2024-08-05 19:19:01 +0200

    add file that Goes through a binary tree using in-order traversal.

--- binary_trees.h
+++ binary_trees.h
@@ -27,5 +27,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif
