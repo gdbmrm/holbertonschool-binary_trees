3dfc8514e51b57ad8e5bae4f70120762b33cd81e
gdbmrm <>
2024-08-06 12:39:59 +0000

    ex 16 binary perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,42 +1,70 @@
 #include "binary_trees.h"
-#include <stdio.h>
-size_t binary_tree_depth(const binary_tree_t *tree)
+
+/**
+ * binary_tree_height - Function that measures
+ * the height of a binary tree
+ *
+ * @tree: Pointer to the root node
+ *
+ * Return: The height of the binary tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t depth = 0;
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	if ((tree->left == NULL) && (tree->right == NULL))
+	{
+		return (0);
+	}
+
+	left_height = binary_tree_height(tree->left);
+
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+	{
+		return (left_height + 1);
+	}
+
+	else
+	{
+		return (right_height + 1);
+	}
+}
+/**
+* binary_tree_is_perfect - Write a function
+* that checks if a binary tree is perfect
+* @tree:  is a pointer to the root node of the tree to check
+* Return: 1 if is perfect else 0
+*/
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t depth_right = 0, depth_left = 0;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	while (tree->parent != NULL)
+
+	depth_right = binary_tree_height(tree->right);
+	depth_left = binary_tree_height(tree->left);
+
+	if (depth_left == depth_right)
 	{
-		depth++;
-		tree = tree->parent;
+		return (1);
 	}
-	return (depth);
-}
 
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-    size_t depth_right = 0, depth_left = 0;
+	if (tree->right == NULL || tree->left == NULL)
+	{
+		return (0);
+	}
+	binary_tree_is_perfect(tree->left);
+	binary_tree_is_perfect(tree->right);
 
-    if (tree == NULL)
-    {
-        return (0);
-        printf("nul");
-    }
-
-    depth_right = binary_tree_depth(tree->right);
-    depth_left = binary_tree_depth(tree->left);
-
-    if (depth_right != depth_left)
-    {
-        return (0);
-        printf("%ld", depth_right);
-        printf("%ld", depth_left);
-    }
-    
-    return (1);
-    printf("parfait");
-    
+	return (0);
 }
--- 16-main.c
+++ 16-main.c
@@ -18,7 +18,7 @@ int main(void)
     binary_tree_insert_right(root->left, 54);
     binary_tree_insert_right(root, 128);
     root->left->left = binary_tree_node(root->left, 10);
-    root->right->left = binary_tree_node(root->right, 10);
+    root->right->left = binary_tree_node(root->right, 15);
 
     binary_tree_print(root);
     perfect = binary_tree_is_perfect(root);
