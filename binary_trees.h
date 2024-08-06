fb8364f1c9a59ce4faa6f5d72dc182448253731a
Ema Decot <emadecot03@gmail.com>
2024-08-06 14:24:32 +0200

    add file that Finds the sibling of a node

--- binary_trees.h
+++ binary_trees.h
@@ -38,4 +38,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_full(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
 #endif
