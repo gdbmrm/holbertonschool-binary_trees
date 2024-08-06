e3320322667acacec55da59240c64efb57d4c230
Ema Decot <emadecot03@gmail.com>
2024-08-06 11:15:36 +0200

    add file that Measures the size of a binary tree.

--- binary_trees.h
+++ binary_trees.h
@@ -13,10 +13,10 @@
  */
 struct binary_tree_s
 {
-  int n;
-  struct binary_tree_s *parent;
-  struct binary_tree_s *left;
-  struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -29,5 +29,6 @@ int binary_tree_is_root(const binary_tree_t *node);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 
 #endif
